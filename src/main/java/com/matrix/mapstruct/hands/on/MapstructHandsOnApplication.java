d0fa17a865f14d402f9bcaa38d2ff0439cfa593a
Felipe Santos <felipe.santos@matrixenergia.com>
2023-05-16 14:03:17 -0300

    initial structure

---
+++ src/main/java/com/matrix/mapstruct/hands/on/MapstructHandsOnApplication.java
@@ -0,0 +1,13 @@
+package com.matrix.mapstruct.hands.on;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstructHandsOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapstructHandsOnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matrix/mapstruct/hands/on/MapstructHandsOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matrix.mapstruct.hands.on;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapstructHandsOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
